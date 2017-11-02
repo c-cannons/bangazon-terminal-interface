ff5de7fc84e3dfa7924dd4e4004425dcc7cc53ba
palmierie <ericmpalmieri@gmail.com>
2017-11-02 12:03:56 -0500

    built basic add customer functionality

--- app/main.rb
+++ app/main.rb
@@ -1,4 +1,13 @@
 require_relative 'controllers/main_menu_controller'
+require_relative 'controllers/customers_controller'
+require_relative 'controllers/orders_controller'
+require_relative 'controllers/products_controller'
+require_relative 'models/customer'
+require_relative 'models/product'
+require_relative 'models/order'
+require_relative 'models/order_line'
+require_relative 'models/payment_type'
+
 
 @main_menu = MainMenuController.new
 @main_menu.display_main_menu
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,1 +1,22 @@
+class Customer
+  attr_accessor :first_name, :last_name, :street_address, :city, :state, :postal_code, :phone_number
 
+  def info(firstname, lastname, street_address, city, state, postal_code, phone_number)
+    @first_name = firstname
+    @last_name = lastname
+    @street_address = street_address
+    @city = city
+    @state = state
+    @postal_code = postal_code
+    @phone_number = phone_number
+  end
+
+  def save
+    puts "SAVE CALLED - This will be pushed the db: #{@first_name}, #{@last_name}, #{@street_address}, #{@city}, #{@state}, #{@postal_code}, #{@phone_number}"
+  end
+
+  def get_customers
+  puts "These are the customers"  
+  end
+    
+end
