c67dce9cef25047a85cd00d5422f23a060051219
palmierie <ericmpalmieri@gmail.com>
2017-11-02 14:22:50 -0500

    connected db

--- app/main.rb
+++ app/main.rb
@@ -7,6 +7,7 @@ require_relative 'models/product'
 require_relative 'models/order'
 require_relative 'models/order_line'
 require_relative 'models/payment_type'
+require 'sqlite3'
 
 
 @main_menu = MainMenuController.new
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,10 @@
 class Customer
   attr_accessor :first_name, :last_name, :street_address, :city, :state, :postal_code, :phone_number
 
+  def initialize
+    @db = SQLite3::Database.open("/Users/ericpalmieri/workspace/bangazon-c-cannons/bangazon-terminal-interface/bangazon_cli.db") 
+  end
+
   def info(firstname, lastname, street_address, city, state, postal_code, phone_number)
     @first_name = firstname
     @last_name = lastname
@@ -12,11 +16,19 @@ class Customer
   end
 
   def save
-    puts "SAVE CALLED - This will be pushed the db: #{@first_name}, #{@last_name}, #{@street_address}, #{@city}, #{@state}, #{@postal_code}, #{@phone_number}"
+    @date = Date.today
+    begin 
+      @db.execute "INSERT INTO Customers VALUES (null,?,?,?,?,?,?,?,'#{@date}',0);", @first_name, @last_name, @street_address, @city, @state, @postal_code, @phone_number
+      
+      puts "Customer #{@first_name} Saved: #{@first_name}, #{@last_name}, #{@street_address}, #{@city}, #{@state}, #{@postal_code}, #{@phone_number}"
+    rescue SQLite3::Exception => e
+      p "Exeception with database query: #{e}"
+      db.rollback
+    end
   end
 
   def get_customers
-  puts "These are the customers"  
+    puts "These are the customers"
   end
     
 end
