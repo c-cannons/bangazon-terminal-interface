e47bef69a2ef2effbbaf1b4c471c32666e301f13
Daniel Certa <daniel.certa.1228@gmail.com>
2017-11-03 15:57:01 -0500

    going home to slowly die

--- spec/models/payment_method_spec.rb
+++ spec/models/payment_method_spec.rb
@@ -10,16 +10,28 @@ describe PaymentMethod do
         end
     end
 
-    context 'When testing add_payment_method' do
+    context 'When testing add_payment_method:' do
 
-        it "should return an array from the database with the correct information confirming the addition" do
+        it "should check that a record for Criminal Bank doesn't exist " do
+            pm = PaymentMethod.new
+            pm_array_1 = pm.list_all_payment_methods
+            expect(pm_array_1.flatten).not_to include("Criminal Bank")
+        end
+
+        it "adds a product and check that the fields are correct" do
             pm = PaymentMethod.new
             pay_method = pm.add_payment_method("Criminal Bank", 1000000000000000)
 
             expect(pay_method).to be_an(Array)
-            expect(pay_method[0][0]).to be_an(Integer)
-            expect(pay_method[0][1]).to eq("Criminal Bank")
-            expect(pay_method[0][2]).to eq(1000000000000000)
+            expect(pay_method.flatten[0]).to be_an(Integer)
+            expect(pay_method.flatten[1]).to eq("Criminal Bank")
+            expect(pay_method.flatten[2]).to eq(1000000000000000)
+        end
+
+        it "should check that a record for Criminal Bank DOES exist " do
+            pm = PaymentMethod.new
+            pm_array_1 = pm.list_all_payment_methods
+            expect(pm_array_1.flatten).to include("Criminal Bank")
         end
 
         it "should have arguments of the correct type" do
