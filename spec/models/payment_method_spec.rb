0b7091926e54edb8d7547396ead11cd232371df8
Nashville Software School <NashvilleSoftwareSchoolMac1@Nashvilles-MacBook-Pro.local>
2017-11-03 12:10:14 -0500

    tests done-ish?

--- spec/models/payment_method_spec.rb
+++ spec/models/payment_method_spec.rb
@@ -11,10 +11,23 @@ describe PaymentMethod do
     end
 
     context 'When testing add_payment_method' do
-        it "should return and integer from the database" do
+
+        it "should return an integer from the database" do
             pm = PaymentMethod.new
             pay_integer = pm.add_payment_method("Criminal Bank", 1000000000000000)
+
             expect(pay_integer).to be_an(Integer)
         end
+
+        it "should have arguments of the correct type" do
+            pm = PaymentMethod.new
+            pay_integer = pm.add_payment_method(@payment_type = "Criminal Bank", @account_number = 1000000000000000)
+
+            expect(@payment_type).to be_a(String)
+            expect(@payment_type).to eq("Criminal Bank")
+            expect(@account_number).to be_an(Integer)
+            expect(@account_number).to eq(1000000000000000)
+        end
     end
+    
 end
