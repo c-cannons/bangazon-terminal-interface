cd4ff234e1a0fe5b9febc6d4798f84fdbcd2cc7e
palmierie <ericmpalmieri@gmail.com>
2017-11-06 15:13:14 -0600

    wrote CustomerController test and updated readme

--- spec/controllers/payment_methods_controller_spec.rb
+++ spec/controllers/payment_methods_controller_spec.rb
@@ -1,34 +1,30 @@
 require './app/controllers/payment_methods_controller.rb'
 
-def setup
-    @pmc = PaymentMethodsController.new(@active_customer = 1)
+describe "initialize" do
+  context "when called" do
+    it "has a valid active_customer" do
+      @pmc = PaymentMethodsController.new(@active_customer = 1)
+      expect(@active_customer).to be_an(Integer)
+      expect(@active_customer).to eq(1)
+    end
+  end
 end
 
 describe "initialize" do
-    context "when called" do
-        it "has a valid active_customer" do
-            setup
-            expect(@active_customer).to be_an(Integer)
-            expect(@active_customer).to eq(1)
-        end
+  context "when called" do
+    it "is a valid instance of PaymentMethodsController" do
+      @pmc = PaymentMethodsController.new(@active_customer = 1)
+      expect(@pmc).to be_an_instance_of(PaymentMethodsController)
     end
-end
-
-describe "initialize" do
-    context "when called" do
-        it "is a valid instance of PaymentMethodsController" do
-            setup
-            expect(@pmc).to be_an_instance_of(PaymentMethodsController)
-        end
-    end
+  end
 end
 
 describe "get_all_payment_methods" do
-    context "when called" do    
-        it "returns an Array from the database" do
-            setup
-            array = @pmc.get_all_payment_methods
-            expect(array).to be_an(Array)
-        end
+  context "when called" do
+    it "returns an Array from the database" do
+      @pmc = PaymentMethodsController.new(@active_customer = 1)
+      array = @pmc.get_all_payment_methods
+      expect(array).to be_an(Array)
     end
+  end
 end
