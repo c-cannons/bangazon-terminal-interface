89fdb7b89ffa9fd80ece36ae2faeb0d0d4a44066
daniel-certa-1228 <mfdancerta@gmail.com>
2017-11-07 15:42:30 -0600

    upsate tests complete

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -51,19 +51,13 @@ class ProductsController
       user_input = gets.chomp
       @product_hash.each do |key, val|
         if user_input.to_s == key.to_s
-          # puts "Do you want to update #{val[2]}? (Y/N)"
-          # next_user_input = gets.chomp
-          # if next_user_input.downcase.to_s == "y"
-            # @product.delete_product(@product_hash[key][0])
-            puts val
-            # prod_array = @product.get_product_by_id(val[0])
-            # puts prod_array
+            # puts val
             @product_id = val[0]
             puts "The current Product Name is '#{val[2]}'.  What would you like the new Product Name to be?"
             @product_name = gets.chomp
             puts "The current Product Price is '#{val[3]}'.  What would you like the new Product Price to be?"
             @product_price = gets.chomp
-            puts puts "The current Product Description is '#{val[4]}'.  What would you like the new Product Description to be?"
+            puts "The current Product Description is '#{val[4]}'.  What would you like the new Product Description to be?"
             @product_desc = gets.chomp
             @product.update_product(@product_id, @active_customer, @product_name, @product_price.to_f, @product_desc)
           # end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,6 +3,7 @@ require_relative "../../app/models/product"
 # variables that will store id integers of the example entries so they can be idetified and deleted
 id_1 = 0
 id_2 = 0
+id_3 = 0
 
 describe '.get_products' do
     context "when calling all products" do
@@ -45,7 +46,7 @@ describe ".add_product - unit" do
     context "when called" do
         it "takes arguments for product name, price and description" do
             product = ProductModel.new
-            new_product = product.add_product(@product_name = "Shoes", @product_price = 9.99, @product_desc = "Nice shoes")
+            new_product = product.add_product(@product_name = "Shoes", @customer_id = 1, @product_price = 9.99, @product_desc = "Nice shoes")
             expect(@product_name).to be_an(String)
             expect(@product_price).to be_a(Float)
             expect(@product_desc).to be_a(String)
@@ -58,6 +59,46 @@ describe ".add_product - unit" do
             product = ProductModel.new
             product.delete_product(id_2)
         end
+    end
+end
+
+describe "update_product" do
+    context "when called" do
+        it "adds a record to be updated during the test" do
+            product = ProductModel.new
+            new_product = product.add_product(@customer_id = 1, @product_name = 'Morphine',  @product_price = 299.99, @product_desc = 'The good stuff.')
+            expect(@product_name).to eq("Morphine")
+            expect(@product_price).to eq(299.99)
+            expect(@product_desc).to eq("The good stuff.")
+            id_3 = new_product.flatten[0]
+        end
+
+        it "confirms that the new test record was added" do
+            product = ProductModel.new
+            new_product = product.get_product_by_id(id_3)
+            expect(new_product.flatten).to include("Morphine")
+            expect(new_product.flatten).to include(299.99)
+            expect(new_product.flatten).to include("The good stuff.")
+        end
+
+        it "updates the new test record successfully" do
+            product = ProductModel.new
+            updated_product = product.update_product(id_3, @customer_id = 2, @product_name = 'Vicodin',  @product_price = 888.88, @product_desc = 'The BAD stuff.')
+        end
+
+        it "confirms that the update has the correct information" do
+            product = ProductModel.new
+            updated_product = product.get_product_by_id(id_3)
+            expect(updated_product.flatten).to include("Vicodin")
+            expect(updated_product.flatten).to include(888.88)
+            expect(updated_product.flatten).to include("The BAD stuff.")
+        end
+
+        it "deletes test product from table" do
+            product = ProductModel.new
+            product.delete_product(id_3)
+        end
+
     end
 end
 
