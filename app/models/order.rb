f731e4dae51f9df2202492a3600ea66c076604db
Hiatt Collins <hiatt.collins.github@gmail.com>
2017-11-07 10:25:46 -0600

    added more order tests and functions

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,22 +2,32 @@ require 'sqlite3'
 
 class OrderModel
 
-  attr_accessor :the_new_order
-
   def initialize
     @db = SQLite3::Database.open(ENV["BANGAZONTI"])
   end
 
+  # function to retrieve all of a customer's orders, active and inactive, from db
   def retrieve_customer_orders(customer_id)
     @all_customer_orders = @db.execute("SELECT * FROM Orders WHERE customer_id = #{customer_id}")
+    return @all_customer_orders
   end
 
+  # function to retrieve order_id of the active customer's active order
+  def retrieve_active_order(customer_id)
+    @active_order_id = @db.execute("SELECT order_id FROM Orders WHERE pay_method_id IS NULL AND customer_id = #{customer_id}")
+    return @active_order_id
+  end
+
+  # function to create a new order
   def new_order(customer_id)
     @db.execute("INSERT INTO Orders VALUES (null, '#{customer_id}', null, '#{Date.today}');")
     return @db.last_insert_row_id
   end
 
-
+  # function to add a pay method to an order, thereby closing it
+  def close_order(pay_method_id, order_id)
+    @db.execute("UPDATE Orders SET pay_method_id = #{pay_method_id} WHERE order_id = #{order_id}")
+  end
 
 
 
